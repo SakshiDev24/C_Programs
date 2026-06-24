67bdfd2abcc6f8b25e430837a810c6cee34884f9
Sakshi Jagtap <sakshijagtap20052005@gmail.com>
2026-06-24 21:31:24 +0530

    Add files via upload

---
+++ Write_file.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main()
+{
+    int fd = 0;
+    int Ret = 0;
+    char Arr[] = "Marvellous Infosystem";
+
+    fd = open("Marvellous.txt",O_RDWR);
+
+    if(fd == -1)
+    {
+        printf("unable to open file\n");
+
+    }
+    else
+    {
+        printf("file gets open with fd : %d\n", fd);
+
+        Ret = write(fd,Arr,10);
+
+        printf("%d bytes gets written into the file\n", Ret);
+    }
+
+    close(fd);
+
+    return 0;
+
+}
